8a2cfe1d6c42c188af5b5780bdda8ccce92c7069
manish7213 <manish.1si11cs057@gmail.com>
2020-05-10 17:14:59 +0530

    Finding duplicates in an array

--- src/com/manish/ds/arrays/ArrayDuplicateFinder.java
+++ src/com/manish/ds/arrays/ArrayDuplicateFinder.java
@@ -5,5 +5,24 @@ package com.manish.ds.arrays;
  */
 public class ArrayDuplicateFinder {
 
+    public static void main(String[] args) {
+        int[] arr = {1,2,8,3,8};
+        printDuplicates(arr);
+    }
+
+    /**
+     *
+     * For now, Solution is only working if elements present are less than the size of array.
+     */
+    private static void printDuplicates(int[] arr) {
+
+        for (int i = 0 ; i<arr.length;i++) {
+            if (arr[Math.abs(arr[i])] > 0) {
+                arr[Math.abs(arr[i])] = -arr[Math.abs(arr[i])];
+            } else {
+                System.out.println(Math.abs(arr[i])+" is a repetition");
+            }
+        }
+    }
 
 }
