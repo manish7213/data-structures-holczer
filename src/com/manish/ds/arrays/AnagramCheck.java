48c907d885c89588cf1956bc7a6c5e88f6d69844
manish7213 <manish.1si11cs057@gmail.com>
2020-05-10 15:19:04 +0530

    Adding "Anagram Check"

---
+++ src/com/manish/ds/arrays/AnagramCheck.java
@@ -0,0 +1,42 @@
+package com.manish.ds.arrays;
+
+import java.util.Arrays;
+
+public class AnagramCheck {
+
+    public static void main(String[] args) {
+        String s1 = "restful";
+        String s2 = "lufster";
+
+        String s3 = "manish";
+        String s4 = "anissm";
+        boolean isAnagram = isAnagram(s3,s4);
+
+        System.out.println(isAnagram);
+    }
+
+    private static boolean isAnagram(String s1, String s2) {
+
+        boolean flag = false;
+        if (s1.length() != s2.length()) {
+            return  false;
+        }
+
+        char[] s1Array = s1.toCharArray();
+        char[] s2Array = s2.toCharArray();
+        Arrays.sort(s1Array);
+        Arrays.sort(s2Array);
+
+        for (int i = 0; i < s1.length() ; i++ ) {
+
+            if (s1Array[i] == s2Array[i]) {
+                flag = true;
+            } else {
+                return false;
+            }
+        }
+
+        return flag;
+    }
+
+}
