b80700e798c49eec39d717186d15f9aafe00ee20
manish7213 <manish.1si11cs057@gmail.com>
2020-05-12 01:30:32 +0530

    LinkedList Implementation for custom objects

--- src/com/manish/ds/linkedList/App.java
+++ src/com/manish/ds/linkedList/App.java
@@ -2,11 +2,23 @@ package com.manish.ds.linkedList;
 
 public class App {
     public static void main(String[] args) {
-        List<Integer> list = new LinkedList<>();
+       /* List<Integer> list = new LinkedList<>();
         list.insert(10);
         list.insert(-2);
         list.insert(3);
         list.insert(100);
-        list.travserseList();
+
+        list.remove(10);
+        list.remove(100);*/
+
+       List<Person> personList = new LinkedList<>();
+       personList.insert(new Person("john",10));
+       personList.insert(new Person("doe",22));
+       personList.insert(new Person("Lily",12));
+       personList.insert(new Person("Shiva",100));
+        System.out.println(personList.size());
+        personList.travserseList();
+
+
     }
 }
